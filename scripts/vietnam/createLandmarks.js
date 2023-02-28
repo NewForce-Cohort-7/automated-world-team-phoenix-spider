b79b59ec5e99bb5b19316100d4e14ec28bbb2f33
JamesB0907 <virverum22@gmail.com>
2023-02-28 13:43:28 -0500

    added vietnam module. fixed header links for Korea and Vietnam

---
+++ scripts/vietnam/createLandmarks.js
@@ -0,0 +1,26 @@
+import { getLandmarks } from './vietnamDB.js'
+
+// We need to export a function to main.js that returns an html string of our landmark elements
+export const createLandmarks = () => {
+
+    // Invoke the function that you imported from the database module
+    const landmarks = getLandmarks()
+
+    // Start building an HTML string with the section title and UL
+    let htmlString = `<h2 class="landmark_title">Popular Landmarks</h2>
+                        <ul class="landmark_list">`
+
+    // Create HTML representations of each landmark here
+    for (const landmark of landmarks) {
+
+        htmlString += `<li class="landmark"> 
+                            <a class="vietnam-link" href="${landmark.address}">${landmark.name}</a> in ${landmark.location}
+                        </li>`
+    }
+
+    // Add the closing tag for the UL
+    htmlString += `</ul>`
+    
+    // return the string we've made
+    return htmlString
+}
